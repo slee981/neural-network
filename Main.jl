db601fa40f986dd6dc56eedceede4cbea687e82c
Stephen Lee <smlee.981@gmail.com>
2020-11-14 18:23:20 -0600

    clearer usage

--- Main.jl
+++ Main.jl
@@ -19,6 +19,8 @@ using MLDatasets
 #############################################################################
 # EXAMPLE 1 - MNIST
 
+# load data 
+
 train_x, train_y = MNIST.traindata()
 test_x,  test_y  = MNIST.testdata()
 
@@ -28,7 +30,7 @@ Y_train = to_categorical(train_y)
 X_test  = reshape(test_x, (size(test_x, 3), 28 * 28))
 Y_test  = to_categorical(test_y)
 
-# SETUP NETWORK - MNIST
+# setup network - mnist
 
 input_size = size(X_train, 2)
 output_size = size(Y_train, 2)
@@ -44,46 +46,47 @@ fit!(net, X_train, Y_train, batchsize=16, epochs=10, learningrate=0.01)
 #############################################################################
 # EXAMPLE 2 - SIMULATION ON KNOWN DATA GENERATING PROCESS
 
-# n = 100000
+# load data 
 
-# c  = ones(n, 1)
-# x1 = [i / 1000 for i = 1:n]
-# x2 = round.(randn(n, 1) * 10)
+n = 100000
 
-# u = randn(n, 1)
+c  = ones(n, 1)
+x1 = [i / 1000 for i = 1:n]
+x2 = round.(randn(n, 1) * 10)
 
-# # dgp 
-# # 
-# # yhat = 4+ 2*x1 - 3*x2 + x1+x2
-# # y    = yhat > mean(yhat) 
-# yhat = 4 * c + 2 * x1 - 10 * x2 + x1 .* x2 + u
+u = randn(n, 1)
 
-# Y = yhat .> 0
-# X = hcat(c, x1, x2)
+# dgp 
+# 
+# yhat = 4 + 2*x1 - 10*x2 + x1+x2
+# y    = yhat > mean(yhat) 
+yhat = 4 * c + 2 * x1 - 10 * x2 + x1 .* x2 + u
 
-# # test and training 
-# trainpct = 0.8
-# ntrain = Int(n * trainpct)
+Y = yhat .> 0
+X = hcat(c, x1, x2)
 
-# shuffledrows = shuffle(1:n)
-# trainrows = shuffledrows[1:ntrain]
-# testrows = shuffledrows[ ntrain + 1:end]
+trainpct = 0.8
+ntrain = Int(n * trainpct)
 
-# X_train = X[trainrows, :]
-# Y_train = Y[trainrows, :]
-# X_test  = X[testrows , :]
-# Y_test  = Y[testrows , :]
+shuffledrows = shuffle(1:n)
+trainrows = shuffledrows[1:ntrain]
+testrows = shuffledrows[ ntrain + 1:end]
 
-# # SETUP NETWORK 
+X_train = X[trainrows, :]
+Y_train = Y[trainrows, :]
+X_test  = X[testrows , :]
+Y_test  = Y[testrows , :]
 
-# input_size = size(X_train, 2)
-# output_size = size(Y_train, 2)
+# setup network 
 
-# net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
-# addlayer!(net, 16, relu, drelu) 
-# addlayer!(net, 4, relu, drelu) 
-# addlayer!(net, output_size, sigmoid, dsigmoid) 
+input_size = size(X_train, 2)
+output_size = size(Y_train, 2)
 
-# # fit network - stochastic gradient descent 
-# fit!(net, X_train, Y_train, batchsize=8, epochs=8, learningrate=0.01)
+net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
+addlayer!(net, 16, relu, drelu) 
+addlayer!(net, 4, relu, drelu) 
+addlayer!(net, output_size, sigmoid, dsigmoid) 
+
+# fit network - stochastic gradient descent 
+fit!(net, X_train, Y_train, batchsize=8, epochs=8, learningrate=0.01)
 
