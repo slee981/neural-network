e9a6e050a169ee7267e25e514fef9d865a703e0f
Stephen Lee <smlee.981@gmail.com>
2020-12-14 07:15:15 -0600

    update main readability

--- Main.jl
+++ Main.jl
@@ -19,74 +19,97 @@ using MLDatasets
 #############################################################################
 # EXAMPLE 1 - MNIST
 
-# load data 
+function example_one() 
 
-train_x, train_y = MNIST.traindata()
-test_x,  test_y  = MNIST.testdata()
+    # load data 
 
-X_train = reshape(train_x, (size(train_x, 3), 28 * 28))
-Y_train = to_categorical(train_y)
+    train_x, train_y = MNIST.traindata()
+    test_x,  test_y  = MNIST.testdata()
 
-X_test  = reshape(test_x, (size(test_x, 3), 28 * 28))
-Y_test  = to_categorical(test_y)
+    X_train = reshape(train_x, (size(train_x, 3), 28 * 28))
+    Y_train = to_categorical(train_y)
 
-# setup network - mnist
+    X_test  = reshape(test_x, (size(test_x, 3), 28 * 28))
+    Y_test  = to_categorical(test_y)
 
-input_size = size(X_train, 2)
-output_size = size(Y_train, 2)
+    # setup network - mnist
 
-net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
-addlayer!(net, 64, relu, drelu) 
-addlayer!(net, 32, relu, drelu) 
-addlayer!(net, output_size, softmax, dsoftmax) 
+    input_size = size(X_train, 2)
+    output_size = size(Y_train, 2)
 
-# fit network - stochastic gradient descent 
-fit!(net, X_train, Y_train, batchsize=16, epochs=10, learningrate=0.01)
+    net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
+    addlayer!(net, 64, relu, drelu) 
+    addlayer!(net, 32, relu, drelu) 
+    addlayer!(net, output_size, softmax, dsoftmax) 
+
+    # fit network - stochastic gradient descent 
+    fit!(net, X_train, Y_train, batchsize=16, epochs=10, learningrate=0.01)
+end
 
 #############################################################################
 # EXAMPLE 2 - SIMULATION ON KNOWN DATA GENERATING PROCESS
 
-# load data 
+function example_two()
 
-n = 100000
+    # create variables
+    n = 100000
 
-c  = ones(n, 1)
-x1 = [i / 1000 for i = 1:n]
-x2 = round.(randn(n, 1) * 10)
+    c  = ones(n, 1)
+    x1 = [i / 1000 for i = 1:n]
+    x2 = round.(randn(n, 1) * 10)
 
-u = randn(n, 1)
+    u = randn(n, 1)
 
-# dgp 
-# 
-# yhat = 4 + 2*x1 - 10*x2 + x1+x2
-# y    = yhat > mean(yhat) 
-yhat = 4 * c + 2 * x1 - 10 * x2 + x1 .* x2 + u
+    # yhat = 4 + 2*x1 - 10*x2 + x1+x2
+    # y    = yhat > mean(yhat) 
+    yhat = 4 * c + 2 * x1 - 10 * x2 + x1 .* x2 + u
 
-Y = yhat .> 0
-X = hcat(c, x1, x2)
+    Y = yhat .> 0
+    X = hcat(c, x1, x2)
 
-trainpct = 0.8
-ntrain = Int(n * trainpct)
+    trainpct = 0.8
+    ntrain = Int(n * trainpct)
 
-shuffledrows = shuffle(1:n)
-trainrows = shuffledrows[1:ntrain]
-testrows = shuffledrows[ ntrain + 1:end]
+    shuffledrows = shuffle(1:n)
+    trainrows = shuffledrows[1:ntrain]
+    testrows = shuffledrows[ ntrain + 1:end]
 
-X_train = X[trainrows, :]
-Y_train = Y[trainrows, :]
-X_test  = X[testrows , :]
-Y_test  = Y[testrows , :]
+    X_train = X[trainrows, :]
+    Y_train = Y[trainrows, :]
+    X_test  = X[testrows , :]
+    Y_test  = Y[testrows , :]
 
-# setup network 
+    # setup network 
 
-input_size = size(X_train, 2)
-output_size = size(Y_train, 2)
+    input_size = size(X_train, 2)
+    output_size = size(Y_train, 2)
 
-net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
-addlayer!(net, 16, relu, drelu) 
-addlayer!(net, 4, relu, drelu) 
-addlayer!(net, output_size, sigmoid, dsigmoid) 
+    net = Network(inputdim=input_size, cost=loss_mse, dcost=dloss_mse)
+    addlayer!(net, 16, relu, drelu) 
+    addlayer!(net, 4, relu, drelu) 
+    addlayer!(net, output_size, sigmoid, dsigmoid) 
 
-# fit network - stochastic gradient descent 
-fit!(net, X_train, Y_train, batchsize=8, epochs=8, learningrate=0.01)
+    # fit network - stochastic gradient descent 
+    fit!(net, X_train, Y_train, batchsize=8, epochs=8, learningrate=0.01)
+end
 
+#############################################################################
+# MAIN 
+#
+# run neural network with MNIST handwriten digit example: 
+# $ julia Main.jl 
+# $ julia Main.jl 1
+# $ julia Main.jl mnist
+#
+# run neural network with known DGP example: 
+# $ julia Main.jl 2
+# $ julia Main.jl dgp
+
+if length(ARGS) == 0 || ARGS[1] == 1 || lowercase(ARGS[1]) == "mnist"
+    example_one()
+elseif ARGS[1] == 2 || lowercase(ARGS[1]) == "dgp"
+    example_two()
+else
+    @info "Not a valid command line argument. Defaulting to the MNIST example."
+    example_one()
+end
